a65a29ddb7a7c5e097d6701ae51ea9b1b310494e
Caleb M. McLaren <kbmclaren@gmail.com>
2022-12-07 19:14:05 -0700

    add imports and contants to bayes.py

---
+++ code/bayes.py
@@ -0,0 +1,14 @@
+# File: bayes.py
+# Author: Caleb M. McLaren with contributions by Lee Vaughan
+# email: Dec. 7th, 2022
+# description: This file must apply Baye's Theorem to the missing sailor problem
+
+import sys
+import random
+import itertools
+import numpy as np
+import cv2 as cv
+
+MAP_FILE = '../resources/cape_python.png'
+
+
